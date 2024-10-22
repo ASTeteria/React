493ee3b1b91bbc36009c56ef42551d05c7565e11
ASTeteria <118740926+ASTeteria@users.noreply.github.com>
2024-10-22 11:49:31 +0000

    edition

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { IPostForm } from '../models/IPostForm';
 import { IPosts } from '../models/IPosts';
 
-export const createPost = (post: IPostForm) => {
+export const createPost = (post: IPostForm): Promise<IPosts> => {
     return axios.post('https://jsonplaceholder.typicode.com/posts', post)
         .then(response => response.data);
 };
-export const getAllPosts = () => {
+export const getAllPosts = (): Promise<IPosts[]> => {
     return axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.data);
 };
